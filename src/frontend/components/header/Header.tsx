f631b6bfc2a56b199793842a88ff1f52652e6f5d
isthisnull <letscodewithzanjiri@hotmail.com>
2024-07-16 23:45:24 +0300

    wtf

--- src/frontend/components/header/Header.tsx
+++ src/frontend/components/header/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import Row from "../common/Row";
 import Link from "next/link";
@@ -10,8 +10,9 @@ const ContainerEl = styled(Row)`
   width: 100%;
   background-color: rgba(0, 0, 0, 0.7);
   color: white;
-  justify-content: center;
-  height: 35px;
+  flex-direction: column;
+  align-items: center;
+  height: 700px;
 `;
 const HeaderCon = styled(Row)`
   min-width: 400px;
@@ -19,6 +20,7 @@ const HeaderCon = styled(Row)`
   font-weight: 300;
   justify-content: space-between;
   align-items: center;
+  background-color: peru;
 `;
 const LinkEl = styled(Row)``;
 const RameshEl = styled(Row)`
@@ -30,38 +32,63 @@ const HomeEl = styled(Row)`
 const AboutEl = styled(Row)`
   cursor: pointer;
 `;
-const LoginEl = styled(Row)`
+const LoginConEl = styled(Row)``;
+const LoginEl = styled(Row)<{ hover?: string }>`
+  z-index: 3;
   cursor: pointer;
+  width: 200px;
+  background-color: ${(e) => (e.hover === "ok" ? "red" : "purple")};
+`;
+const LoginHoverEl = styled(Row)<{ hover?: string }>`
+  color: black;
+  width: 100%;
+  z-index: ${(e) => (e.hover === "ok" ? "-1" : "-1")};
+  left: 0;
+  top: 0;
+  background-color: rgb(243, 52, 52);
+  height: ${(e) => (e.hover === "ok" ? "300px" : "00px")};
+  /* transition: 600ms all; */
 `;
 const HomeCon = styled(Row)`
+  z-index: 3;
   gap: 4px;
 `;
 function Header() {
+  const [hovered, sethovered] = useState("");
   return (
-    <ContainerEl>
-      <HeaderCon>
-        <LinkEl>
-          <Link href="#">
-            <HomeCon>
-              <RameshEl>Ramesh Logo</RameshEl>
-              <HomeEl>Home</HomeEl>
-            </HomeCon>
-          </Link>
-        </LinkEl>
-        <LinkEl>
-          <Link href="#">
-            <AboutEl>About Us</AboutEl>
-          </Link>
-        </LinkEl>
-        <LinkEl>
-          <Link href="#">
-            <LoginEl>
-              <FontAwesomeIcon icon={faUser} size="lg" />
-            </LoginEl>
-          </Link>
-        </LinkEl>
-      </HeaderCon>
-    </ContainerEl>
+    <>
+      <ContainerEl>
+        <HeaderCon onMouseEnter={() => sethovered("ok")}>
+          <LinkEl>
+            <Link href="#">
+              <HomeCon>
+                <RameshEl>Ramesh Logo</RameshEl>
+                <HomeEl>Home</HomeEl>
+              </HomeCon>
+            </Link>
+          </LinkEl>
+          <LinkEl>
+            <Link href="/about">
+              <AboutEl onMouseEnter={() => sethovered("o2k")}>About Us</AboutEl>
+            </Link>
+          </LinkEl>
+          <LinkEl>
+            <Link href="#">
+              <LoginConEl>
+                <LoginEl onMouseEnter={() => sethovered("ok")} hover={hovered}>
+                  <FontAwesomeIcon icon={faUser} size="lg" />
+                </LoginEl>
+              </LoginConEl>
+            </Link>
+          </LinkEl>
+        </HeaderCon>
+        <LoginHoverEl
+          onMouseLeave={() => sethovered("ok")}
+          hover={hovered}
+        ></LoginHoverEl>
+      </ContainerEl>
+      <HeaderCon>as</HeaderCon>
+    </>
   );
 }
 
