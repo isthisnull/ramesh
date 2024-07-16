4bd08fa3b98dc22b747f07b8b0efa5ef3bb449fe
isthisnull <letscodewithzanjiri@hotmail.com>
2024-07-17 00:17:14 +0300

    hover

--- src/frontend/components/header/Header.tsx
+++ src/frontend/components/header/Header.tsx
@@ -6,21 +6,26 @@ import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
 
-const ContainerEl = styled(Row)`
+const ContainerEl = styled(Row)<{ hover?: string }>`
   width: 100%;
-  background-color: rgba(0, 0, 0, 0.7);
+  background-color: rgba(0, 0, 0, 0.3);
   color: white;
   flex-direction: column;
   align-items: center;
-  height: 700px;
+  height: auto;
+  position: absolute;
+  z-index: 1;
 `;
 const HeaderCon = styled(Row)`
   min-width: 400px;
-  width: 700px;
+  width: 100%;
+  /* padding-right: 40px; */
+  /* max-width: 700px; */
   font-weight: 300;
-  justify-content: space-between;
+  justify-content: space-evenly;
   align-items: center;
-  background-color: peru;
+  height: 36px;
+  background-color: purple;
 `;
 const LinkEl = styled(Row)``;
 const RameshEl = styled(Row)`
@@ -31,64 +36,57 @@ const HomeEl = styled(Row)`
 `;
 const AboutEl = styled(Row)`
   cursor: pointer;
+  background-color: blue;
 `;
 const LoginConEl = styled(Row)``;
 const LoginEl = styled(Row)<{ hover?: string }>`
-  z-index: 3;
   cursor: pointer;
-  width: 200px;
-  background-color: ${(e) => (e.hover === "ok" ? "red" : "purple")};
 `;
 const LoginHoverEl = styled(Row)<{ hover?: string }>`
   color: black;
   width: 100%;
-  z-index: ${(e) => (e.hover === "ok" ? "-1" : "-1")};
-  left: 0;
-  top: 0;
+  z-index: 1;
   background-color: rgb(243, 52, 52);
-  height: ${(e) => (e.hover === "ok" ? "300px" : "00px")};
-  /* transition: 600ms all; */
+  height: ${(e) => (e.hover === "ok" ? "220px" : "0px")};
+  transition: ease-out 400ms all;
 `;
 const HomeCon = styled(Row)`
   z-index: 3;
   gap: 4px;
 `;
 function Header() {
-  const [hovered, sethovered] = useState("");
+  const [hovered, sethovered] = useState("asas");
   return (
-    <>
-      <ContainerEl>
-        <HeaderCon onMouseEnter={() => sethovered("ok")}>
-          <LinkEl>
-            <Link href="#">
-              <HomeCon>
-                <RameshEl>Ramesh Logo</RameshEl>
-                <HomeEl>Home</HomeEl>
-              </HomeCon>
-            </Link>
-          </LinkEl>
-          <LinkEl>
-            <Link href="/about">
-              <AboutEl onMouseEnter={() => sethovered("o2k")}>About Us</AboutEl>
-            </Link>
-          </LinkEl>
-          <LinkEl>
-            <Link href="#">
-              <LoginConEl>
-                <LoginEl onMouseEnter={() => sethovered("ok")} hover={hovered}>
-                  <FontAwesomeIcon icon={faUser} size="lg" />
-                </LoginEl>
-              </LoginConEl>
-            </Link>
-          </LinkEl>
-        </HeaderCon>
-        <LoginHoverEl
-          onMouseLeave={() => sethovered("ok")}
-          hover={hovered}
-        ></LoginHoverEl>
-      </ContainerEl>
-      <HeaderCon>as</HeaderCon>
-    </>
+    <ContainerEl onMouseEnter={() => sethovered(`${hovered}`)}>
+      <HeaderCon onMouseEnter={() => sethovered(`${hovered}`)}>
+        <LinkEl>
+          <Link href="#">
+            <HomeCon>
+              <RameshEl>Ramesh Logo</RameshEl>
+              <HomeEl>Home</HomeEl>
+            </HomeCon>
+          </Link>
+        </LinkEl>
+        <LinkEl>
+          <Link href="/about">
+            <AboutEl onMouseEnter={() => sethovered("o2k")}>About Us</AboutEl>
+          </Link>
+        </LinkEl>
+        <LinkEl>
+          <Link href="#">
+            <LoginConEl>
+              <LoginEl onMouseEnter={() => sethovered("ok")} hover={hovered}>
+                <FontAwesomeIcon icon={faUser} size="lg" />
+              </LoginEl>
+            </LoginConEl>
+          </Link>
+        </LinkEl>
+      </HeaderCon>
+      <LoginHoverEl
+        onMouseLeave={() => sethovered("o2k")}
+        hover={hovered}
+      ></LoginHoverEl>
+    </ContainerEl>
   );
 }
 
